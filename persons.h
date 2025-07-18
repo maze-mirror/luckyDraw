5b2a53d39dc7e79799488f69f8e81249cf93a0b5
xiaoyufu910 <3180218068@qq.com>
2025-07-18 08:45:21 +0800

    Update persons.h

--- persons.h
+++ persons.h
@@ -13,10 +13,21 @@ class persons : public QWidget
 
 public:
     explicit persons(QWidget *parent = nullptr);
+    // 设置抽奖人数
+    void setLotteryNumber(int number);
+    // 获取当前设置的抽奖人数
+    int getLotteryNumber() const;
+
+    // 弹出设置抽奖人数的对话框
+    int showSettingDialog();
     ~persons();
+signals:
+    // 当抽奖人数设置改变时发出的信号
+    void lotteryNumberChanged(int newNumber);
 
 private:
     Ui::persons *ui;
+    int m_lotteryNumber; // 存储抽奖人数
 };
 
 #endif // PERSONS_H
