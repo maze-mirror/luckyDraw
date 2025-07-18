72785ab5a0254b669c0d7f406e2d42db391a772a
xiaoyufu910 <3180218068@qq.com>
2025-07-18 08:30:08 +0800

    Update lists.h

--- lists.h
+++ lists.h
@@ -2,7 +2,32 @@
 #define LISTS_H
 
 #include <QWidget>
-
+#include <QStandardItemModel>
+#include <QTableView>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QAction>
+#include <QStatusBar>
+#include <QTimer>
+#include <QDateTime>
+#include <QTextStream>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QFontDialog>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QComboBox>
+#include <QLabel>
+#include <QHeaderView>
+#include<QInputDialog>
+//#include<QMediaPlay>
 namespace Ui {
 class lists;
 }
@@ -14,9 +39,31 @@ class lists : public QWidget
 public:
     explicit lists(QWidget *parent = nullptr);
     ~lists();
-
+private slots:
+    void addPerson();
+    void editPerson();
+    void deletePerson();
+    void importFromText();
+    void importFromCSV();
+    void saveProject();
+    void loadProject();
+    void updateStatus();
 private:
     Ui::lists *ui;
+    void setupUI();
+    void setupMenus();
+    void setupToolbars();
+    void setupStatusBar();
+    void setupCentralWidget();
+
+    QStandardItemModel *personModel;
+    QTableView *personTableView;
+    QLineEdit *nameLineEdit;
+    QPushButton *addButton;
+    QPushButton *editButton;
+    QPushButton *deleteButton;
+    QLabel *statusLabel;
+    QTimer *statusTimer;
 };
 
 #endif // LISTS_H
